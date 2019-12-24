5854739c47c282e7794519321793bfe6b5b9d9fa
eeghor <eeghor@gmail.com>
2019-12-24 23:48:10 +1100

    plain python

---
+++ likely_buyers.py
@@ -0,0 +1,38 @@
+from sklearn.pipeline import Pipeline
+import pandas as pd
+from collections import Counter, defaultdict
+import arrow
+import numpy as np
+
+from sklearn.base import TransformerMixin
+from sklearn.compose import ColumnTransformer
+
+class PropensityEstimator:
+
+	def __init__(self):
+		
+		self.vars = {'cat': 'FromDayWeek ToDayWeek QuoteWeek QuoteDay QuoteHour Lang ResCountry'.split()}
+
+	def load_data(self, tr_file='B2C_Rentalcover_24DEC2019.csv'):
+
+		self.d = pd.read_csv('data/' + tr_file, 
+					parse_dates=['FromDate','ToDate','CreatedOn', 'CreatedOnDate'])
+
+		print(pd.get_dummies(self.d.iloc[:30][self.vars['cat']], prefix='is'))
+
+		return self
+
+if __name__ == '__main__':
+	
+	pe = PropensityEstimator().load_data()
+
+	print(pe.d) 
+
+# class HourOfDayTransformer(TransformerMixin):
+
+#     def transform(self, X, **transform_params):
+#         hours = DataFrame(X['datetime'].apply(lambda x: x.hour))
+#         return hours
+
+#     def fit(self, X, y=None, **fit_params):
+#         return self
