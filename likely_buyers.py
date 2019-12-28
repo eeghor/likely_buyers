724632ce0cef432de19684a2df9c69b13faf3857
Unknown <eeghor@gmail.com>
2019-12-28 23:49:57 +1100

    added transformer

--- likely_buyers.py
+++ likely_buyers.py
@@ -9,12 +9,27 @@ from sklearn.compose import ColumnTransformer
 
 class PreviousBookings(TransformerMixin):
 
-    def transform(self, X, **transform_params):
-        X.sort_values(['CustomerId', 'CreatedOn'])
-        return hours
+	"""
+	extract total previous bookings for each customers (for every booking or transaction)
+	"""
 
-    def fit(self, X, y=None, **fit_params):
-        return self
+	def transform(self, X, **kwargs):
+
+		X_ = X[['CustomerId', 'Type', 'BookingId', 'CreatedOn']].sort_values(['CustomerId', 'CreatedOn'])
+		X_['isBooking'] = X_['Type'].apply(lambda x: 1 if x == 'Booking' else 0)
+		X_['isQuote'] = X_['isBooking'].apply(lambda x: 0 if x == 1 else 1)
+
+		prevs = []
+
+		for r in X_[['CustomerId', 'isBooking', 'isQuote']].groupby('CustomerId'):
+			
+			prevs.append(r[1].cumsum().shift(1, fill_value=0)[['isBooking', 'isQuote']] \
+				 .rename(columns={'isBooking': 'prev_bks', 'isQuote': 'prev_qts'}))
+
+		return pd.concat(prevs)
+
+	def fit(self, X, y=None, **kwargs):
+		return self
 
 class PropensityEstimator:
 
@@ -27,7 +42,7 @@ class PropensityEstimator:
 		self.d = pd.read_csv('data/' + tr_file, 
 					parse_dates=['FromDate','ToDate','CreatedOn', 'CreatedOnDate'])
 
-		print(pd.get_dummies(self.d.iloc[:30][self.vars['cat']], prefix='is'))
+		# print(pd.get_dummies(self.d.iloc[:30][self.vars['cat']], prefix='is'))
 
 		return self
 
@@ -35,5 +50,7 @@ if __name__ == '__main__':
 	
 	pe = PropensityEstimator().load_data()
 
-	print(pe.d) 
+	pl = PreviousBookings().transform(pe.d)
+
+	print(pl)
 
