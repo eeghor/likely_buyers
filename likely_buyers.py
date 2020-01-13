af714cd43ce8ac8dd3fc2bb5c19894e91168c78a
Unknown <eeghor@gmail.com>
2020-01-14 08:36:54 +1100

    more transforms

--- likely_buyers.py
+++ likely_buyers.py
@@ -7,16 +7,16 @@ from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.cluster import KMeans
 import pandas as pd
 from collections import Counter, defaultdict
-from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, FunctionTransformer
 import arrow
 import numpy as np
 import json
+import calendar
 
 from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.compose import ColumnTransformer
 
 outbound_trips = json.load(open('data/outbound_trips.json'))
-
 cnt_lefthand = pd.read_csv('data/countries_lefthand.csv')
 
 # competitor_prices = {'AU': json.load(open('data/prices_pday_au.json'))}
@@ -148,181 +148,20 @@ class CountryIsLeftHand(BaseEstimator, TransformerMixin):
 	def fit(self, X, y=None):
 		return self
 
-class ToFromCountries(BaseEstimator, TransformerMixin):
+class CountryFrequentlyBooked(BaseEstimator, TransformerMixin):
 
 	"""
-	where is a customer renting a car and what's his country or residence
+	returns a score describing how frequently this country is booked
 	"""
+
+	def __init__(self, score_dict):
+		self.score_dict = score_dict
 
 	def transform(self, X):
 
-		top30_to = ['AU', 'US', 'IT', 'GB', 'ES', 'CA', 'NZ', 'DE', 'FR', 'IS', 'PT', 
-					'GR', 'JP', 'IE', 'CH', 'NL', 'ZA', 'MX', 'AT', 'TH', 'PL', 'HR', 
-					'CY', 'IL', 'NO', 'RO', 'BE', 'TW', 'CZ', 'HU']
-
-		to_country_names = ['tocountry_' + c for c in top30_to] + ['tocountry_XX']
-
-		top30_from = ['AU', 'GB', 'RU', 'US', 'CA', 'IL', 'TW', 'ES', 'HK', 'SG', 'IT',
-					  'FR', 'DE', 'CH', 'NZ', 'NL', 'JP', 'KR', 'UA', 'PL', 'MY', 'AE', 
-					  'BE', 'IE', 'CN', 'BR', 'AR', 'TH', 'IN', 'CZ']
-
-		from_country_names = ['rescountry_' + c for c in top30_from] + ['rescountry_XX']
-
-		cs = {'C1': ['AU', 'NZ', 'US', 'GB', 'CA', 'IE'],
-			  'C2': ['IT', 'ES', 'FR', 'PT', 'GR', 'CY', 'HR', 'ME', 'TR'],
-			  'C3': ['UA', 'RU', 'MD', 'BY', 'PL', 'CZ', 'HU'],
-			  'C4': ['DE', 'CH', 'NL', 'SE', 'AT', 'FI', 'NO', 'DK']}
-
-		_Y1 = X['ToCountry'].apply(lambda x: x if x in top30_to else 'XX')
-		_Y2 = X['ResCountry'].apply(lambda x: x if x in top30_from else 'XX')
-
-		_X1 = pd.get_dummies(_Y1, prefix='tocountry')
-		_X2 = pd.get_dummies(_Y2, prefix='rescountry')
-
-		for n in to_country_names:
-			if n not in _X1.columns:
-				_X1[n] = 0
-
-		for n in from_country_names:
-			if n not in _X2.columns:
-				_X2[n] = 0
-
-		_X3 = pd.DataFrame({'tocountry_lh': X['ToCountry'].isin(cnt_lefthand['iso_code'])*1})
-		_X4 = pd.DataFrame({'rescountry_lh': X['ResCountry'].isin(cnt_lefthand['iso_code'])*1})
-
-		# cnt_cols = 'ToCountry ResCountry'.split()
-
-		# _X = pd.concat([pd.get_dummies(X[c], prefix=c.lower()) for c in cnt_cols], sort=False, axis=1)
-		# _X['US_tour_dest'] = X['ToCountry'].apply(lambda x: 1 if x in touristic_dests['US'] else 0)
-		# _X['AU_tour_dest'] = X['ToCountry'].apply(lambda x: 1 if x in touristic_dests['AU'] else 0)
-		# _X5 = _Y1.apply(lambda x: 1 if x in touristic_dests['UK'] else 0)
-		_X5 = pd.DataFrame({'pop_dest_uk': _Y1.apply(lambda x: outbound['UK'][x] if x in outbound['UK'] else 0)})
-		# _X['from_lh_country'] = X['ResCountry'].isin(cnt_lefthand['iso_code'])*1
-		# _X['to_lh_country'] = X['ToCountry'].isin(cnt_lefthand['iso_code'])*1
-		# _X['NZ_tour_dest'] = X['ToCountry'].apply(lambda x: 1 if x in touristic_dests['NZ'] else 0)
-		# _X['RU_tour_dest'] = X['ToCountry'].apply(lambda x: 1 if x in touristic_dests['RU'] else 0)
-
-		# all_dummy_names = sorted(['_'.join([cl.lower(), c]) for c in 'C1 C2 C3 C4 XX'.split() for cl in cnt_cols])
-
-		# for c in all_dummy_names:
-		# 	if c not in _X.columns:
-		# 		_X[c] = 0
-
-		return pd.concat([_X1, _X2, _X3, _X4, _X5], axis=1, sort=False)
-
+		return X.iloc[:,0].apply(lambda _: self.score_dict.get(str(_), 0)).values.reshape(len(X),1)
 
 	def fit(self, X, y=None):
-		return self
-
-class CustomerDetails(BaseEstimator, TransformerMixin):
-
-	"""
-	basic customer details
-	"""
-
-	def transform(self, X):
-
-		lang_names = sorted(['lang_' + l for l in 'en de fr es ru jp xx'.split()])
-
-		s = pd.get_dummies(X['Lang'].apply(lambda x: x if x in 'en de fr es ru jp'.split() else 'xx'), prefix='lang')
-
-		for nm in lang_names:
-			if nm not in s.columns:
-				s[nm] = 0
-
-		return s[lang_names]
-
-	def fit(self, X, y=None):
-		return self
-
-class QuoteTiming(BaseEstimator, TransformerMixin):
-
-	"""
-	mostly time of the quote or booking
-	"""
-
-	def transform(self, X):
-
-		qmonth_names = ['quotemonth_' + m for m in sorted('Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split())]
-		qweek_names = ['quoteweek_' + str(c) for c in range(1,53)]
-		qday_names = ['quoteday_' + d for d in 'Sun Mon Tue Wed Thu Fri Sat'.split()]
-		qhr_names = [f'quotehour_{h:02}' for h in range(25)]
-
-		dummy_months = pd.get_dummies(X['QuoteMonth'], prefix='quotemonth')
-		dummy_weeks = pd.get_dummies(X['QuoteWeek'], prefix='quoteweek')
-		dummy_days = pd.get_dummies(X['QuoteDay'], prefix='quoteday')
-		# dummy_hrs = pd.get_dummies(X['QuoteHour'])
-
-		for w in qmonth_names:
-			if w not in dummy_months.columns:
-				dummy_months[w] = 0
-
-		dummy_months = dummy_months[qmonth_names]
-
-		for w in qweek_names:
-			if w not in dummy_weeks.columns:
-				dummy_weeks[w] = 0
-
-		dummy_weeks = dummy_weeks[qweek_names]
-
-		for w in qday_names:
-			if w not in dummy_days.columns:
-				dummy_days[w] = 0
-
-		dummy_days = dummy_days[qday_names]
-
-		# for w in qhr_names:
-		# 	if w not in dummy_hrs.columns:
-		# 		dummy_hrs[w] = 0
-
-		# dummy_hrs = dummy_hrs[qhr_names]
-
-		return pd.concat([dummy_months, dummy_weeks], sort=False, axis=1)
-
-	def fit(self, X, y=None):
-		return self
-
-class PrevQtsThisTrip(BaseEstimator, TransformerMixin):
-
-	"""
-	extract total previous bookings for each customers (for every booking or transaction)
-	"""
-
-	def transform(self, X, **kwargs):
-
-		prevs = [] 
-
-		for r in X.groupby('CustomerId'):
-
-			d = r[1].sort_values('CreatedOn')
-
-			prev_bks = []
-			prev_qts = []
-			prev_cnl = []
-			
-			for i, cr in enumerate(d.iterrows()):
-
-				this_day = cr[1]['CreatedOnDate']
-
-				prev_actv_this_country = d.iloc[:i]
-
-				if not prev_actv_this_country.empty:
- 
-					prev_bks_this_country = max(sum([arrow.get(c).shift(days=-2) <= arrow.get(this_day) <= arrow.get(c).shift(days=+2) 
-															for c in prev_actv_this_country[prev_actv_this_country['isBooking'] == 1]['FromDate']]),
-												sum([arrow.get(c).shift(days=-2) <= arrow.get(this_day) <= arrow.get(c).shift(days=+2) 
-															for c in prev_actv_this_country[prev_actv_this_country['isBooking'] == 1]['ToDate']]))
-
-				else:
-					prev_bks_this_country = 0
-
-				prev_bks.append(prev_bks_this_country)
-
-			prevs.append(pd.DataFrame({'prev_qts_same_trip': prev_bks}).set_index(d.index))
-
-		return pd.concat(prevs)
-
-	def fit(self, X, y=None, **kwargs):
 		return self
 
 class DataLoader:
@@ -339,7 +178,12 @@ class DataLoader:
 
 		self.data = pd.read_csv('data/' + file, parse_dates=self.cols_to_parse_date)
 
+		bkcount_to = Counter(self.data[self.data['isBooking']==1]['ToCountry'])
+		tot_bookings = sum(bkcount_to.values())
+		self.bkscore_to = {c: round(bkcount_to[c]/tot_bookings,3) for c in bkcount_to}
+
 		if countries:
+			print(f'only customers from {", ".join(countries)}')
 			self.data = self.data[self.data['ResCountry'].isin(countries)]
 
 		self.data['dest_popul'] = self.data[['ResCountry', 'ToCountry']] \
@@ -371,9 +215,24 @@ if __name__ == '__main__':
 								('ct', ColumnTransformer([('trip_details', 
 									 							ColumnAsIs(), 
 									 							['DurationDays', 'UpfrontDays', 'Cancelled']),
+														  ('quote_month', 
+									 							OneHotEncoder(handle_unknown='ignore'), 
+									 							['QuoteMonth']),
+														  ('quote_week', 
+									 							OneHotEncoder(handle_unknown='ignore'), 
+									 							['QuoteWeek']),
+														  ('quote_day', 
+									 							OneHotEncoder(handle_unknown='ignore'), 
+									 							['QuoteDay']),
 														  ('tocountry_lefthand',
 														  		CountryIsLeftHand(),
 														  		['ToCountry']),
+														  ('country_freq_booked',
+														  		CountryFrequentlyBooked(dl.bkscore_to),
+														  		['ToCountry']),
+														  ('webs_lang',
+														  		OneHotEncoder(handle_unknown='ignore'),
+														  		['Lang']),
 														  ('rescountry_lefthand',
 														  		CountryIsLeftHand(),
 														  		['ResCountry']),
@@ -392,9 +251,6 @@ if __name__ == '__main__':
 														  		['Paid', 'Coupon'])
 														]))
 								])
-							 	# ('to_from_countries', ToFromCountries()), 
-							 	# ('cust_details', CustomerDetails()), 
-							 	# ('quote_timing', QuoteTiming()), 
 							 	# ('potential_savings', PotentialSavings())
 							 	# ])
 
@@ -426,7 +282,7 @@ if __name__ == '__main__':
 
 	# print(f'accuracy: {accuracy_score(y_test, y_h):06.4f}')
 
-	# print(classification_report(y_test, y_h))
+	print(classification_report(y_test, y_pred))
 
 	# print(confusion_matrix(y_test, y_h))
 
